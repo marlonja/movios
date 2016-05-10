a9c2d8637af8a5fd731a781738719d6864d26379
Johannes Klint <johannesklint@gmail.com>
2016-05-10 10:46:34 +0200

    Added missing files in in angular

--- Movios 2016.05.03/JS/serverCall.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('test', []);
-app.controller('mainControl', function($http, $scope){
-
-  var urlBase="http://localhost:8080/api/movies/";
- 
-        $http.get(urlBase).success(function(data){
-          $scope.title = data;   
-        });        
-});
