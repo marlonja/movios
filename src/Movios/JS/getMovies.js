f841954ed173bdeb6e510dd55f1e97af72aae987
marlonja <jakobssonmarlon@gmail.com>
2016-05-17 09:59:40 +0200

    sort genres

--- src/Movios/JS/getMovies.js
+++ src/Movios/JS/getMovies.js
@@ -16,17 +16,14 @@ app.controller('mainControl', function($http, $scope){
         });
     };
 
-    $scope.getMoviesByGenre = function(){
-        console.log("inne i getMoviesByGenre");
+    $scope.getMoviesByGenre = function(genre){
+        console.log(genre);
         $http.get(urlBase).success(function(data){
             var list = [];
             for(i = 0; i<data.length; i++){
-                if(data[i].genre == "ACTION"){
+                if(data[i].genre == genre){
                     list.push(data[i]);
-
                 }
-
-
             }
 
             $scope.actionMovies = list;
